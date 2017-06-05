8083a87525959a67cc2137f98799391af558cddf
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-06-05 03:04:52 -0300

    admin views

--- app/models/spree/enquiry.rb
+++ app/models/spree/enquiry.rb
@@ -1,3 +1,7 @@
 class Spree::Enquiry < ApplicationRecord
-	enum state: [ :pending, :concluded ]
+  enum state: [ :pending, :concluded ]
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
